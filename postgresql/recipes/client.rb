71a1bbef55fa0c84a148513745c7be4689a2a9e8
samirjha123 <skjecnit@gmail.com>
2014-07-10 10:40:02 +0530

    Update client.rb

--- postgresql/recipes/client.rb
+++ postgresql/recipes/client.rb
@@ -2,10 +2,6 @@ if platform_family?('ubuntu', 'debian') && node['postgresql']['version'].to_f > 
   node.default['postgresql']['enable_pgdg_apt'] = true
 end
 
-if(node['postgresql']['enable_pgdg_apt'])
-  include_recipe 'postgresql::apt_pgdg_postgresql'
-end
-
 if(node['postgresql']['enable_pgdg_yum'])
   include_recipe 'postgresql::yum_pgdg_postgresql'
 end
