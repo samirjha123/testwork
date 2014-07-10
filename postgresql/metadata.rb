957de81305cfc251fd153db5197206532823ea49
samirjha123 <skjecnit@gmail.com>
2014-07-10 13:07:17 +0530

    Update metadata.rb

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -5,7 +5,7 @@ license           "Apache 2.0"
 description       "Installs and configures postgresql for clients or servers"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "3.4.1"
-
+recipe            "postgresql", "Includes postgresql::client"
 recipe            "postgresql::client", "Installs postgresql client package(s)"
 %w{ubuntu debian fedora suse amazon}.each do |os|
   supports os
