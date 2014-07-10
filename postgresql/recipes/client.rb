7f62068476b06371a8d09dde2d157b82aa7b25f0
samirjha123 <skjecnit@gmail.com>
2014-07-10 14:22:37 +0530

    Update client.rb

--- postgresql/recipes/client.rb
+++ postgresql/recipes/client.rb
@@ -1,15 +1,7 @@
 pg_packages = case node['platform']
-when "ubuntu","debian"
-  %w{postgresql-client libpq-dev}
-when "fedora","suse","amazon"
+
+when "amazon"
   %w{postgresql-devel}
-when "redhat","centos","scientific"
-  case
-  when node['platform_version'].to_f >= 6.0
-    %w{postgresql-devel}
-  else
-    [ "postgresql#{node['postgresql']['version'].split('.').join}-devel" ]
-  end
 end
 
 pg_packages.each do |pg_pack|
