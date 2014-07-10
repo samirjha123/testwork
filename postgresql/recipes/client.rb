54d1bcb787c14b0e32271ad5ed07b787c5473f35
samirjha123 <skjecnit@gmail.com>
2014-07-10 13:43:40 +0530

    Update client.rb

--- postgresql/recipes/client.rb
+++ postgresql/recipes/client.rb
@@ -1,13 +1,23 @@
-if platform_family?('ubuntu', 'debian') && node['postgresql']['version'].to_f > 9.3
-  node.default['postgresql']['enable_pgdg_apt'] = true
+pg_packages = case node['platform']
+when "ubuntu","debian"
+  %w{postgresql-client libpq-dev}
+when "fedora","suse","amazon"
+  %w{postgresql-devel}
+when "redhat","centos","scientific"
+  case
+  when node['platform_version'].to_f >= 6.0
+    %w{postgresql-devel}
+  else
+    [ "postgresql#{node['postgresql']['version'].split('.').join}-devel" ]
+  end
 end
 
-if(node['postgresql']['enable_pgdg_yum'])
-  include_recipe 'postgresql::yum_pgdg_postgresql'
+pg_packages.each do |pg_pack|
+  package pg_pack do
+    action :nothing
+  end.run_action(:install)
 end
 
-node['postgresql']['client']['packages'].each do |pg_pack|
-
-  package pg_pack
-
-end
+gem_package "pg" do
+  action :nothing
+end.run_action(:install)
