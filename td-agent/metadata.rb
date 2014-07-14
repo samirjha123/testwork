65ad38efbbbdae66911c6b09dae0de07be463ada
samirjha123 <skjecnit@gmail.com>
2014-07-14 17:32:52 +0530

    Update metadata.rb

--- td-agent/metadata.rb
+++ td-agent/metadata.rb
@@ -7,10 +7,11 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe           "td-agent", "td-agent configuration"
 
-%w{redhat centos debian ubuntu}.each do |os|
+%w{redhat amazon centos debian ubuntu}.each do |os|
   supports os
 end
 
+#depends 'apt'
 depends 'yum'
 
 attribute "td_agent/api_key",
