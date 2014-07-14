3a12f29723ec13781a6a5dd32c635145c8a81407
samirjha123 <skjecnit@gmail.com>
2014-07-14 16:57:22 +0530

    Create remi.rb

---
+++ yum/recipes/remi.rb
@@ -0,0 +1,16 @@
+include_recipe "yum::epel"
+
+yum_key node['yum']['remi']['key'] do
+  url  node['yum']['remi']['key_url']
+  action :add
+end
+
+yum_repository "remi" do
+  description "Les RPM de remi pour Enterprise Linux #{node['platform_version']} - $basearch"
+  key node['yum']['remi']['key']
+  mirrorlist node['yum']['remi']['url']
+  failovermethod "priority"
+  includepkgs node['yum']['remi']['includepkgs']
+  exclude node['yum']['remi']['exclude']
+  action :create
+end
