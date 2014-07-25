b711f27bcac37be31a03de631e8d29d665db7429
samirjha123 <skjecnit@gmail.com>
2014-07-25 15:11:10 +0530

    add td-agent previous

--- td-agent/recipes/default.rb
+++ td-agent/recipes/default.rb
@@ -29,16 +29,6 @@ directory '/etc/td-agent/' do
   action :create
 end
 
-directory "/etc/profile.d" do
-  owner   "root"
-  mode    "0755"
-end
-
-template "/etc/profile.d/td-agent.sh" do
-  owner   "root"
-  mode    "0755"
-end
-
 case node['platform']
 when "ubuntu"
   dist = node['lsb']['codename']
@@ -47,30 +37,50 @@ when "ubuntu"
     uri source
     distribution dist
     components ["contrib"]
+    key "http://packages.treasure-data.com/debian/RPM-GPG-KEY-td-agent"
     action :add
   end
 when "centos", "redhat", "amazon"
   yum_repository "treasure-data" do
     url "http://packages.treasure-data.com/redhat/$basearch"
+#    gpgkey "http://packages.treasure-data.com/redhat/RPM-GPG-KEY-td-agent"
     action :add
   end
-end
-
-package "td-agent" do
-  action :install
 end
 
 template "/etc/td-agent/td-agent.conf" do
   mode "0644"
   source "td-agent.conf.erb"
-  notifies :restart, 'service[td-agent]'
+end
+
+if node['td_agent']['includes']
+  directory "/etc/td-agent/conf.d" do
+    mode "0755"
+  end
+end
+
+package "td-agent" do
+  action :upgrade
+  version node[:td_agent][:version] if node[:td_agent][:pinning_version]
+end
+
+node[:td_agent][:plugins].each do |plugin|
+  if plugin.is_a?(Hash)
+    plugin_name, plugin_attributes = plugin.first
+    td_agent_gem plugin_name do
+      plugin true
+      %w{action version source options gem_binary}.each do |attr|
+        send(attr, plugin_attributes[attr]) if plugin_attributes[attr]
+      end
+    end
+  elsif plugin.is_a?(String)
+    td_agent_gem plugin do
+      plugin true
+    end
+  end
 end
 
 service "td-agent" do
   action [ :enable, :start ]
-end
-
-template "/etc/logrotate.d/td-agent" do
-  mode "0644"
-  source "td-agent.logrotate.erb"
+  subscribes :restart, resources(:template => "/etc/td-agent/td-agent.conf")
 end
